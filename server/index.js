8bc4d4514234737b00a0af1cf5bf86fbba54533d
im-vedant <194vedantgupta@gmail.com>
2024-03-19 02:39:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   
 app.use(cookie_parser())
 app.use(cors( {
-    origin :"https://studynotion-backend-kgfl.onrender.com",
+    origin :"https://studynotion-frontend-zeta-indol.vercel.app/",
     credentials : true
 }))
 app.use(fileUpload({useTempFiles : true, tempFileDir:'/tmp'}))
