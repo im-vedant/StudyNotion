07619134ddd5ce77939491197bc4973b5a8fe1e4
im-vedant <194vedantgupta@gmail.com>
2024-03-28 00:26:27 +0530

    Update otpModel.js

--- server/models/otpModel.js
+++ server/models/otpModel.js
@@ -10,12 +10,9 @@ const otpSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default : Date.now(),
-    expires : 60*1000*60
-  }
+  
 });
+otpSchema.index({ createdAt: 1 }, { expireAfterSeconds: 300 })
 async function sendVerificationEmail(email,otp)
 {
     try{
